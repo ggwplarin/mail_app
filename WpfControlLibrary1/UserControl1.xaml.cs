39962e3a079f83f4ad44a0a8a7bea937d1ddc9f2
al8137962 <al8137962@gmail.com>
2020-07-10 09:21:06 +0300

    UwU

--- WpfControlLibrary1/UserControl1.xaml.cs
+++ WpfControlLibrary1/UserControl1.xaml.cs
@@ -13,47 +13,100 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace WpfControlLibrary1
 {
     /// <summary>
     /// Логика взаимодействия для UserControl1.xaml
     /// </summary>
+
+    [Serializable]
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Service { get; set; }
+
+
+        public User(string login, string password, string service)
+        {
+            Login = login;
+            Password = password;
+            Service = service;
+
+        }
+    }
     public partial class UserControl1 : UserControl
     {
-        string path;
+
+        User user;
+        string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "users.dat");
         public UserControl1()
         {
-            path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                if (fs.Length > 0)
+                {
+                    user = (User)formatter.Deserialize(fs);
+                }
+            }
+            
             InitializeComponent();
+            if (user != null)
+            {
+                LoginBox.Text = user.Login;
+                PassBox.Password = user.Password;
+                Service.Text = user.Service;
+            }
+
         }
 
         private void Send_Click(object sender, RoutedEventArgs e)
         {
-            // отправитель - устанавливаем адрес и отображаемое в письме имя
-            System.Net.Mail.MailAddress from = new MailAddress(LoginBox.Text, "gg");
-            // кому отправляем
-            System.Net.Mail.MailAddress to = new MailAddress(To.Text);
-            // создаем объект сообщения
-            System.Net.Mail.MailMessage m = new System.Net.Mail.MailMessage(from, to);
-            // тема письма
-            m.Subject = Theme.Text;
-            // текст письма
-            m.Body = Body.Text;
-            // письмо представляет код html
-            m.IsBodyHtml = false;
-            // адрес smtp-сервера и порт, с которого будем отправлять письмо
-            SmtpClient smtp = new SmtpClient(Service.Text, 587);
-            // логин и пароль
-            smtp.Credentials = new NetworkCredential(LoginBox.Text, PassBox.Password);
+            try
+            {
+                // отправитель - устанавливаем адрес и отображаемое в письме имя
+                System.Net.Mail.MailAddress from = new MailAddress(LoginBox.Text, "gg");
+                // кому отправляем
+                System.Net.Mail.MailAddress to = new MailAddress(To.Text);
+                // создаем объект сообщения
+                System.Net.Mail.MailMessage m = new System.Net.Mail.MailMessage(from, to);
+                // тема письма
+                m.Subject = Theme.Text;
+                // текст письма
+                m.Body = Body.Text;
+                // письмо представляет код html
+                m.IsBodyHtml = false;
+                // адрес smtp-сервера и порт, с которого будем отправлять письмо
+                SmtpClient smtp = new SmtpClient(Service.Text, 587);
+                // логин и пароль
+                smtp.Credentials = new NetworkCredential(LoginBox.Text, PassBox.Password);
 
-            smtp.EnableSsl = true;
-            smtp.Send(m);
+                smtp.EnableSsl = true;
+                smtp.Send(m);
+                MessageBox.Show("сообщение отправлено!");
+            }
+            catch
+            {
+                MessageBox.Show("всё сломався!");
+            }
         }
 
         private void Save_Click(object sender, RoutedEventArgs e)
         {
+            BinaryFormatter formatter = new BinaryFormatter();
+            
+                user = (new User(LoginBox.Text,
+                    PassBox.Password, Service.Text));
+
+
+                using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+                {
+                    formatter.Serialize(fs, user);
+                }
             
         }
     }
